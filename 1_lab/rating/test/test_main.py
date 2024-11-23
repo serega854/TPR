426ccd455e0a818d7c4434b4c596282445b90a4d
QuanRy <kacman1964@mail.ru>
2024-11-23 04:21:55 +0300

    добавим в файл test_main путь к src.main

--- 1_lab/rating/test/test_main.py
+++ 1_lab/rating/test/test_main.py
@@ -1,4 +1,10 @@
 # -*- coding: utf-8 -*-
+import sys
+import os
+
+# Добавляем корневую директорию проекта в PYTHONPATH
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
 from src.main import get_path_from_arguments
 import pytest
 
