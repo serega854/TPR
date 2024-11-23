84beaeee50d44cde4cd1020eb0c698f95948ea0a
QuanRy <kacman1964@mail.ru>
2024-11-23 04:24:20 +0300

    вернем, как было

--- 1_lab/rating/test/test_main.py
+++ 1_lab/rating/test/test_main.py
@@ -1,10 +1,4 @@
 # -*- coding: utf-8 -*-
-import sys
-import os
-
-# Добавляем корневую директорию проекта в PYTHONPATH
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
-
 from src.main import get_path_from_arguments
 import pytest
 
