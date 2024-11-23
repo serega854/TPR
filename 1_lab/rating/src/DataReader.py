b98f75f2616ba8191f17b41d02e20b4801218261
QuanRy <85188407+QuanRy@users.noreply.github.com>
2024-11-23 10:06:05 +0300

    Update DataReader.py добавил класс JSONDataReader с наследованием методов от DataReader

--- 1_lab/rating/src/DataReader.py
+++ 1_lab/rating/src/DataReader.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 from .Types import DataType
 from abc import ABC, abstractmethod
+import json
 
 
 class DataReader(ABC):
@@ -8,3 +9,9 @@ class DataReader(ABC):
     @abstractmethod
     def read(self) -> DataType:
         pass
+
+class JSONDataReader (DataReader):
+    def read(self, file_path):
+        with open(file_path, 'r') as file:
+            data = json.load(file)
+        return data
