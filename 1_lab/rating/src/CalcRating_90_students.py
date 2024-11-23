65d4b3fc876e5bf99a1198221026f3a48a2554b8
QuanRy <85188407+QuanRy@users.noreply.github.com>
2024-11-23 10:59:06 +0300

    Update CalcRating_90_students.py добавил функцию, которая считает студентов,  у которых есть два минимум предмета с оценкой 90 баллов и выше

--- 1_lab/rating/src/CalcRating_90_students.py
+++ 1_lab/rating/src/CalcRating_90_students.py
@@ -1,1 +1,26 @@
+# -*- coding: utf-8 -*-
+from .Types import DataType
 
+RatingType = dict[str, float]
+
+
+class CalcRating_90_studets:
+
+    def __init__(self, data: DataType) -> None:
+        self.data: DataType = data
+        self.rating: RatingType = {}
+
+    def calc_90_rating(self) -> RatingType:
+        key_answer
+        for key in self.data:
+            count = 0
+            self.rating[key] = 0.0
+            for subject in self.data[key]: 
+                self.rating[key] += subject[1]
+                if self.rating[key] >= 90:
+                  count++
+                if count >= 2:
+                  key_answer = key
+                  
+        return key_answer
+        
